81980ec22e056e15a96eba1bc4a835d062c92f5e
tender-ops <ayodejibabalola@outlook.com>
2022-10-03 20:17:05 +0000

    updated

--- employee-card.java
+++ employee-card.java
@@ -1,7 +1,11 @@
+<<<<<<< HEAD
  HEAD
 hello, this is for master branch
 =======
 hello,this is for DEV branch
 walmartDEV
+=======
+hello,this is for DEV branch
+>>>>>>> 16c5369 (updated)
 This is EC file...
 for senior employers.
